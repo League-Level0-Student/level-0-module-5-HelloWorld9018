3cc563c06c6439cba4ab65797dee35658e5aeb7b
CalebVia <55600946+CalebVia@users.noreply.github.com>
2020-02-13 15:58:56 -0800

    forloop

--- src/_05_snowflake/Snowflake.java
+++ src/_05_snowflake/Snowflake.java
@@ -6,25 +6,48 @@ package _05_snowflake;
 
 import java.awt.Color;
 
+import org.jointheleague.graphical.robot.Robot;
+
 
 public class Snowflake {
 	
 	public static void main(String[] args) {
 
 		// #1. Make a new Robot
-
+	
+Robot bo = new Robot();
 
 		// #2. Set your robot’s position to x=300 and y=300
-
+bo.setX(300);
+bo.setY(300);
 
 		// #3. Put the robot's pen down
+bo.setPenWidth(5000);
+bo.sparkle();
 
+bo.penDown();
 
 		// #4. Set the robot’s speed to 5
-
+bo.setSpeed(5000);
 
 		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6 times (use i as the counter)
-
+for(int i = 1; i<7; i++) {
+	bo.setPenColor(0,0,0);
+	bo.move(50);
+	bo.turn(60);
+	if (i%2==0) {
+		bo.setPenColor(Color.BLUE);
+	}
+	else {
+		bo.setPenColor(Color.RED);
+	
+	}
+	for(int e = 1; e<7; e++) {
+		bo.turn(-60);
+		bo.move(50);
+	}
+	bo.setPenColor(0,0,0);
+}
 			
 			// #6. Set the pen color to black
 
---
+++ src/_06_for_loop_gauntlet/ForLoop.java
@@ -0,0 +1,20 @@
+package _06_for_loop_gauntlet;
+
+public class ForLoop {public static void main(String[] args) {
+	for(int i = 0; i <= 100; i ++) {
+		System.out.println(i);
+	}
+	for(int i = 100; i >= 0; i --) {
+		System.out.println(i);
+	}
+	for(int i = 2; i <= 100; i ++) {
+		
+		if(i%2==0) {
+			System.out.println(i);
+		}
+	}
+	
+	
+}
+
+}
